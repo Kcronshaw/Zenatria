8772a5e40470fe948bec6577c43737e96a6346e0
DerpDragon <52836715+DerpDragon@users.noreply.github.com>
2023-10-03 22:57:49 -0400

    spawning enemies

---
+++ Zenatria/Assets/Scripts/Spawner.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+
+    public int EnemiesAlive = 0;
+    public Transform spawnPoint;
+
+    private int waveIndex;
+    public Wave[] waves;
+
+    private void Update()
+    {
+
+        if (EnemiesAlive > 0)
+        {
+            return;
+        }
+
+        if (Input.GetKeyDown("space"))
+        {
+            StartCoroutine(SpawnWave());
+        }
+
+    }
+
+
+
+    IEnumerator SpawnWave()
+    {
+        waveIndex++;
+        Debug.Log(waveIndex);
+
+        for (int i = 0; i < waveIndex; i++)
+        {
+
+            EnemiesAlive = waves[i].count;
+
+            for (int n = 0; n < waves[i].count; n++)
+            {
+                SpawnEnemy(waves[i].enemy);
+
+                yield return new WaitForSeconds(waves[i].rate);
+            }
+
+        }
+
+
+    }
+
+    void SpawnEnemy(GameObject enemy)
+    {
+        GameObject enemyUnit = Instantiate(enemy, transform, transform);
+        enemyUnit.GetComponent<Enemy>().spawner = this;
+    }
+
+    public void EnemyKilled()
+    {
+        EnemiesAlive--;
+        if (EnemiesAlive <= 0)
+        {
+            TowerBuilder.instance.money += waves[waveIndex - 1].cashGen;
+        }
+
+    }
+
+}
---
+++ Zenatria/Assets/Scripts/TowerBuilder.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TowerBuilder : MonoBehaviour
+{
+    public static TowerBuilder instance;
+
+    public int money = 50;
+    private void Awake()
+    {
+        //lets other scripts refernce this without needing to hard code it, or use inspector
+        if (instance == null) //in case 2 tower builders are put in somehow
+        {
+            instance = this;
+        }
+
+    }
+
+
+    public GameObject normalTowerPrefab;
+
+    private void Start()
+    {
+        
+    }
+
+
+    public void createTower(int towerToBuild)
+    {
+        switch (towerToBuild)
+        {
+            case 1:
+                Instantiate(normalTowerPrefab);
+                break;
+            default:
+                break;
+        }
+    }
+
+}
